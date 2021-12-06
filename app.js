bbd66dfa88134b04d2a6b779d1911d8003e858b9
Mark Eubans <markeubans@marks-air.lan>
2021-12-06 16:02:43 +0800

    initial

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/express/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/image', function (req, res) {
+    res.sendFile(path.join(__dirname + '/express/image.html'));
+});
+
+router.get('/location', function (req, res) {
+    res.sendFile(path.join(__dirname + '/express/location.html'));
+});
+
+router.get('/marker', function (req, res) {
+    res.sendFile(path.join(__dirname + '/express/marker.html'));
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
